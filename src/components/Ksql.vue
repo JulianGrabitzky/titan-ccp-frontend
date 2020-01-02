fd6b678797ed766a9b95f9d3a33751eca2137277
Julian Grabitzky <stu209185@mail.uni-kiel.de>
2020-01-02 20:19:05 +0100

    add the ksql vue comp

---
+++ src/components/Ksql.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-container>
+      <b-row class="mb-4">
+        <b-col>
+          <show-topic></show-topic>
+        </b-col>
+      </b-row>
+      <b-row class="mb-4">
+        <b-col>
+          <show-stream></show-stream>
+        </b-col>
+      </b-row>
+      <b-row class="mb-4">
+        <b-col>
+          <show-table></show-table>
+        </b-col>
+      </b-row>
+      <b-row class="mb-4">
+        <b-col>
+          <show-query></show-query>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import ColorRepository from "../ColorRepository";
+import ShowTopic from "./ksql-components/ShowTopic.vue";
+import ShowStream from "./ksql-components/ShowStream.vue";
+import ShowTable from "./ksql-components/ShowTable.vue";
+import ShowQuery from "./ksql-components/ShowQuery.vue";
+
+@Component({
+  components: {
+    ShowTopic,
+    ShowStream,
+    ShowTable,
+    ShowQuery
+  }
+})
+export default class Ksql extends Vue {}
+</script>
+
